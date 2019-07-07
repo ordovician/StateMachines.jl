be805b3bd92054ecfccf5f791a0f0f722ede1e5f
Erik Engheim <erik.engheim@mac.com>
2019-07-07 15:25:50 +0200

    Initial commit

---
+++ README.md
@@ -0,0 +1,5 @@
+# Beginnings of a State Machine Package
+This is an unfinnished package for a State Machine in Julia. It has multiple design goals:
+
+1. A simple and easy to understand implementation. Simplicity is more important than features.
+2. Register actions upon transitioning from one particular state to another as well upon entering a particular state or exiting a particular state.
--- src/StateMachines.jl
+++ src/StateMachines.jl
@@ -14,9 +14,18 @@ Transition(state::State, event::Event) where {State, Event} = Transition(state, 
 mutable struct FSM{State, Event}
     state::State
     transitions::Array{Transition{State, Event}}
+    exit_callbacks::Dict{State, Function}
+    transition_callbacks::Dict{Int64}   # Index same as transitions.
+    enter_callbacks::Dict{State, Function}
 end
 
-FSM{State, Event}(state::State) where {State, Event} = FSM(state, Transition{State, Event}[])
+function FSM{State, Event}(state::State) where {State, Event}
+    FSM(state, 
+        Transition{State, Event}[], 
+        Dict{State, Function}(),
+        Dict{Int64, Function}(), 
+        Dict{State, Function}())
+end
 
 function add_transition!(fsm::FSM{State, Event}, s0::State, event::Event, s1::State) where {State, Event}
     push!(fsm.transitions, Transition(s0, event, s1))
@@ -42,9 +51,43 @@ end
 function fire!(fsm::FSM{State, Event}, event::Event) where {State, Event}
     r = searchsorted(fsm.transitions, Transition(fsm.state, event))
     if !isempty(r)
-       fsm.state = fsm.transitions[first(r)].s1 
+        i = first(r)
+        prev = fsm.state
+        transition = fsm.transitions[i]
+        fsm.state = transition.s1
+        
+        # chech if there are any registered callbaks for when exiting state s0
+        if haskey(fsm.exit_callbacks, transition.s0)
+            fsm.exit_callbacks[transition.s0](transition)
+        end
+        
+        # check if there are callbacks registered for a transition from
+        # state s0 to s1 with event.
+        if haskey(fsm.transition_callbacks, i)
+            fsm.transition_callbacks[i](transition)
+        end
+        
+        # any callbacks for when entering state s1?
+        if haskey(fsm.enter_callbacks, transition.s1)
+            fsm.enter_callbacks[transition.s1](transition)
+        end
     end
     fsm.state
+end
+
+function on_enter!(f::Function, fsm::FSM{State, Event}, s1::State) where {State, Event}
+    fsm.on_enter[s1] = f
+end
+
+function on_transition!(f::Function, fsm::FSM{State, Event}, s0::State, event::Event) where {State, Event}
+    r = searchsorted(fsm.transitions, Transition(fsm.state, event))
+    if !isempty(r)
+        i = first(r)
+    end
+end
+
+function on_exit!(f::Function, fsm::FSM{State, Event}, s0::State) where {State, Event}
+    fsm.exit_callbacks[s0] = f
 end
 
 end # module
